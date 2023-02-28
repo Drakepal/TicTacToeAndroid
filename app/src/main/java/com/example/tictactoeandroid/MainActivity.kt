e48871d5b52bbee6511f8b5e1c9664ab69ac81ee
Drakepal <playonplay23@gmail.com>
2023-02-28 13:28:45 -0600

    the x and o appears

--- app/src/main/java/com/example/tictactoeandroid/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeandroid/MainActivity.kt
@@ -2,10 +2,88 @@ package com.example.tictactoeandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.tictactoeandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    enum class Turn {
+        O,
+        X
+    }
+
+    private var firstTurn = Turn.X
+    private var currentTurn = Turn.X
+    private var boardList = mutableListOf<Button>()
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBoard()
+    }
+
+    private fun initBoard() {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+    }
+
+    fun boardTapped(view: View) {
+        if (view !is Button)
+            return
+        addToBoard(view)
+
+        if(fullBoard()) {
+
+        }
+    }
+
+    private fun fullBoard(): Boolean {
+        
+    }
+
+    private fun addToBoard(button: Button) {
+        if(button.text != "")
+            return
+
+        if(currentTurn == Turn.O) {
+            button.text= "O"
+            currentTurn = Turn.X
+        }
+
+        else if(currentTurn == Turn.X) {
+            button.text = "X"
+            currentTurn = Turn.O
+        }
+
+        setTurnLabel()
+    }
+
+    private fun setTurnLabel() {
+        var turnText = ""
+
+        if(currentTurn == Turn.X)
+            turnText = "Turn $X"
+
+        else if (currentTurn == Turn.O)
+            turnText = "Turn $O"
+
+        binding.turnTV.text = turnText
+    }
+
+    companion object {
+        const val O = "O"
+        const val X = "X"
     }
 }
