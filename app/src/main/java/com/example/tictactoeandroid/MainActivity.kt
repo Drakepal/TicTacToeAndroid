f594fa89a73067b5894d593faef702f1965eeaca
Drakepal <playonplay23@gmail.com>
2023-03-01 12:07:16 -0600

    app is done

--- app/src/main/java/com/example/tictactoeandroid/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeandroid/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private var firstTurn = Turn.X
     private var currentTurn = Turn.X
     private var boardList = mutableListOf<Button>()
+    private var scoreX = 0
+    private var scoreO = 0
 
 
     private lateinit var binding: ActivityMainBinding
@@ -45,10 +47,46 @@ class MainActivity : AppCompatActivity() {
             return
         addToBoard(view)
 
+        if (checkedForWinner(O)) {
+            scoreO++
+            result("O Wins!")
+        }
+        else if (checkedForWinner(X)) {
+            scoreX++
+            result("X Wins!")
+        }
+
+
         if(fullBoard()) {
             result("Draw")
         }
     }
+
+    private fun checkedForWinner(s: String): Boolean {
+        if(match(binding.a1,s) && match(binding.a2,s) && match(binding.a3,s))
+            return true
+        if(match(binding.b1,s) && match(binding.b2,s) && match(binding.b3,s))
+            return true
+        if(match(binding.c1,s) && match(binding.c2,s) && match(binding.c3,s))
+            return true
+
+        if(match(binding.a1,s) && match(binding.b1,s) && match(binding.c1,s))
+            return true
+        if(match(binding.a2,s) && match(binding.b2,s) && match(binding.c2,s))
+            return true
+        if(match(binding.a3,s) && match(binding.b3,s) && match(binding.c3,s))
+            return true
+
+        if(match(binding.a1,s) && match(binding.b2,s) && match(binding.c3,s))
+            return true
+        if(match(binding.a3,s) && match(binding.b2,s) && match(binding.c1,s))
+            return true
+
+        return false
+    }
+
+
+    private fun match(button: Button, symbol: String) = button.text == symbol
 
     private fun resetBoard() {
         for(button in boardList) {
@@ -64,8 +102,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun result(title: String) {
+        val message = "\nTotal Wins for O: $scoreO \n\nTotal Wins for X: $scoreX"
         AlertDialog.Builder(this)
             .setTitle(title)
+            .setMessage(message)
             .setPositiveButton("New Game") {
                 _,_ ->
                 resetBoard()
